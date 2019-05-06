ce284c87056f739d74ddee2ca3df500d09e8c5b3
Raj Khandekar <khandekarp@outlook.com>
2019-05-06 18:33:23 -0400

    able to add ingredients

---
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.BuildControl.Less}>Less</button>
+        <button className={classes.BuildControl.More} onClick ={props.added}>More</button>
+    </div>
+);
+
+export default buildControl;
--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -5,12 +5,22 @@ import classes from'./Burger.css';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    // transform key value pairs to array
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return[...Array(props.ingredients[igKey])].map((_,i)=>{ // transform key value pairs to array
+            return[...Array(props.ingredients[igKey])].map((_,i)=>{ 
                 return <BurgerIngredients key={igKey + i} type={igKey}/>;
             }); //array with 2 elements
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    
+        if (transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
+
+    console.log(transformedIngredients);
 
     return(
         <div className = {classes.Burger}>
