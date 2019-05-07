3671691da725d42206c6d03de0e1881e6b152769
Raj Khandekar <khandekarp@outlook.com>
2019-05-07 00:15:47 -0400

    disable btn when no ingredients to remove

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -9,14 +9,15 @@ const controls = [
     { label: 'Meat', type: 'meat'},
 ];
 
-const buildControls = (props) =>(
-    <div className ={classes.BuildControls}>
+const buildControls = (props) => (
+    <div className = {classes.BuildControls}>
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
                 label={ctrl.label}
                 added = {() => props.ingredientAdded(ctrl.type)}
-                removed = {() => props.ingredientRemoved(ctrl.type)}/>
+                removed = {() => props.ingredientRemoved(ctrl.type)}
+                disabled = {props.disabled[ctrl.type]}/>
         ))}
     </div>
 );
