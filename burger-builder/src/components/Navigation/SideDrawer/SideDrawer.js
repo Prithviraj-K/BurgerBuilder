d6305e0599521f40ac6e42a52bff151dcdd4681a
Raj Khandekar <khandekarp@outlook.com>
2019-05-09 20:03:38 -0400

    side navigation for mobile complete

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import Logo from'../../Logo/Logo';
+import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
-const sideDrawer = (props) =>{
-    //...
+const sideDrawer = (props) => {
+    
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
 
-    return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo/>
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
