657ea50a22b17f3cd2f692fb89e3b2bcec58b660
Raj Khandekar <khandekarp@outlook.com>
2019-05-09 23:00:01 -0400

    backdrop fixed in desktop mode

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
@@ -24,7 +24,7 @@ class Layout extends Component {
         return (
             <Aux>
                 <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer 
+                <SideDrawer y
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
