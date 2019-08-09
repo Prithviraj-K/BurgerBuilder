4684d7267b536646c67fd041bdaba6ccb12e4aa4
Raj Khandekar <khandekarp@outlook.com>
2019-08-08 21:43:44 -0400

    routing added and fetching data from firebase backend

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
-import classes from'./Burger.css';
+import classes from './Burger.css';
 
 const burger = (props) => {
-
+    console.log(props);
     // transform key value pairs to array
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return[...Array(props.ingredients[igKey])].map((_,i)=>{ 
-                return <BurgerIngredients key={igKey + i} type={igKey}/>;
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredients key={igKey + i} type={igKey} />;
             }); //array with 2 elements
         })
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
-    
-        if (transformedIngredients.length === 0){
-            transformedIngredients = <p>Please start adding ingredients!</p>
-        }
 
-    return(
-        <div className = {classes.Burger}>
-            <BurgerIngredients type="bread-top"/>
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredients type="bread-top" />
             {transformedIngredients}
-            <BurgerIngredients type="bread-bottom"/>
+            <BurgerIngredients type="bread-bottom" />
         </div>
     );
 };
 
-export default burger;
+export default withRouter(burger);
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,6 +26,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://react-my-burger-ec202.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ ingredients: response.data });
@@ -85,29 +86,16 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //alert ('You continue!');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Prithviraj Khandekar',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '32q134',
-                    country: 'Canada'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     };
 
     render() {
