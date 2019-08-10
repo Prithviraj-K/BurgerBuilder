0cac70cb03661a199a24cd37e1a3241fb54b95cc
Raj Khandekar <khandekarp@outlook.com>
2019-08-09 21:42:20 -0400

    can input in forms

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -83,8 +83,16 @@ class ContactData extends Component {
             });
     }
 
-    inputChangedHandler = (event) => {
-        console.log(event.target.value);
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm={
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm});
     }
 
     render() {
@@ -103,7 +111,7 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value} 
-                        changed={this.inputChangedHandler}/>
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
